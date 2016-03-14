9dce2094f88a2b8ef66f125cc0ac57e06ec5969b
mylittilepig <sunkevin1991@hotmail.com>
2016-03-14 20:19:48 +0100

    add annuaires and information and some changes

---
+++ information.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>X-Chine, Ecole Polytechnique, Palaiseau</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/blueimp-gallery.min.css">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light' rel='stylesheet' type='text/css'>
+        <link href="css/style.css" rel="stylesheet">
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/bootstrap.js"></script>
+        <script src="js/code.js"></script>
+        <script src="js/jquery.min.js"></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+        	<div class="row">
+        		<div class="col-md-10"><img src="photos/logo.jpeg" class="img-rounded" alt="Logo" height="100" width="150"></div>
+        		<div class="col-md-2"><img src="photos/china.jpg" class="img-rounded" alt="china" height="100" width="150" align="top"></div>
+<!--         		<div class="col-md-2"><img src="photos/chinadragon.gif" class="img-rounded" alt="dragon" height="75" width="120" align="left"></div> -->
+        	</div>
+        	<div class="row">
+        		<div class="col-md-1" style="border:1px; padding:5px;margin:5px"></div>
+        		<div class="col-md-2" style="border:1px; padding:5px;margin:5px">Culture</div>
+        		<div class="col-md-2" style="border:1px; padding:5px;margin:5px">History</div>
+        		<div class="col-md-2" style="border:1px; padding:5px;margin:5px">Nourriture</div>
+        		<div class="col-md-2" style="border:1px; padding:5px;margin:5px">Stage</div>
+        	</div>
+
+
+
+        </div>
+
+
+
+
+
+    </body>
+
+
+
+</html>
